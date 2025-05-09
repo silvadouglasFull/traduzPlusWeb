a311bc00484c2b50e8056784e008c5d0b649a31f
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-09 16:04:51 -0300

    add styles in app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
