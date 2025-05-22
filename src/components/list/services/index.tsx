7b8a2f13f112d011e5af9e1742b6fe9ac229221b
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-22 15:09:27 -0300

    feat: refactor ServicesList and Item components; replace CardGroup with Row and adjust layout for better responsiveness

--- src/components/list/services/index.tsx
+++ src/components/list/services/index.tsx
@@ -1,13 +1,13 @@
 import { Item } from "@components/list/services/item"
 import type { ServiceListProps } from "@components/list/services/types"
 import type React from "react"
-import { CardGroup } from "react-bootstrap"
+import { Row } from "react-bootstrap"
 export const ServicesList: React.FC<ServiceListProps & React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>> = ({ items }) => {
     return (
-        <CardGroup>
+        <Row className="justify-content-start align-items-start">
             {items.length ? items.map(item => (
                 <Item key={item.id} {...item} />
             )) : null}
-        </ CardGroup>
+        </ Row>
     )
 }
--- src/components/list/services/item/index.tsx
+++ src/components/list/services/item/index.tsx
@@ -1,25 +1,29 @@
 import { Icon } from "@components/icons"
 import type { Item as TItem } from "@components/list/services/item/types"
 import React from "react"
-import { Card } from "react-bootstrap"
+import { Card, Col } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import { Paragraph } from "./paragraph"
 import './styles.css'
 export const Item: React.FC<TItem> = ({ descriptions, icon, name, link, textButon, }) => {
     return (
-        <Card className="m-2 card-services-list rounded-4">
-            <Card.Body>
-                <Icon name={icon} className="mb-4 icon" />
-                <Card.Title className="title-color">
-                    {name}
-                </Card.Title>
-                {descriptions.length ? descriptions.map(item => (<Paragraph key={item.id} {...item} />)) : null}
-            </Card.Body>
-            <Card.Footer>
-                <Link to={link} className="btn btn-dark">
-                    {textButon}
-                </Link>
-            </Card.Footer>
-        </Card>
+        <Col className="col-12 mb-3" md={6}>
+            <Card style={{
+                height: 418
+            }} className="rounded-4">
+                <Card.Body className="p-5">
+                    <Icon name={icon} className="mb-4 icon" />
+                    <Card.Title className="title-color">
+                        {name}
+                    </Card.Title>
+                    {descriptions.length ? descriptions.map(item => (<Paragraph key={item.id} {...item} />)) : null}
+                    <Card.Footer>
+                        <Link to={link} className="btn btn-dark">
+                            {textButon}
+                        </Link>
+                    </Card.Footer>
+                </Card.Body>
+            </Card>
+        </Col>
     )
 }
