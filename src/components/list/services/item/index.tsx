1b95db2ed3818a1db78ab140347c79acb64726a4
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-22 17:06:12 -0300

    feat: add logo image and update Item component for improved styling and responsiveness

--- src/components/list/services/item/index.tsx
+++ src/components/list/services/item/index.tsx
@@ -10,15 +10,17 @@ export const Item: React.FC<TItem> = ({ descriptions, icon, name, link, textButo
         <Col className="col-12 mb-3" md={6}>
             <Card style={{
                 height: 418
-            }} className="rounded-4">
+            }} className="rounded-4" bg="dark">
                 <Card.Body className="p-5">
                     <Icon name={icon} className="mb-4 icon" />
-                    <Card.Title className="title-color">
+                    <Card.Title className="text-uppercase">
                         {name}
                     </Card.Title>
                     {descriptions.length ? descriptions.map(item => (<Paragraph key={item.id} {...item} />)) : null}
-                    <Card.Footer>
-                        <Link to={link} className="btn btn-dark">
+                    <Card.Footer style={{
+                        backgroundColor: 'transparent'
+                    }}>
+                        <Link to={link} className="btn w-100 btn-secondary">
                             {textButon}
                         </Link>
                     </Card.Footer>
