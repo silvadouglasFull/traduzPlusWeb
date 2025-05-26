4f01453a3d3eb11cd60471e0093d8f0ad26cdbf6
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-26 15:46:59 -0300

    feat: update FirstSection layout to display contact information in a structured format

--- src/pages/aboutUs/layouts/firstSection/index.tsx
+++ src/pages/aboutUs/layouts/firstSection/index.tsx
@@ -1,10 +1,36 @@
+import { contacts } from "@flavor/constants/contacts";
+import { formatPhoneNumber } from "@utils/form/mask/phone";
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Card, Col, Container, Row } from "react-bootstrap";
 export const FirstSection: React.FC = () => {
     return (
         <section className="mb-3">
             <Container>
-
+                <Row className="mt-5 justify-content-center justify-content-lg-between flex-lg-row-reverse align-items-center">
+                    {contacts.phones.map(item => (
+                        <Col className="text-center" sm={12} md={6} key={item.key}>
+                            <Card className="border border-top-0 border-left-0 border-right-0">
+                                <Card.Body>
+                                    {formatPhoneNumber(item.number, "br")}
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                    <Col sm={12} md={6} className="text-center">
+                        <Card className="border border-top-0 border-left-0 border-right-0">
+                            <Card.Body>
+                                {contacts.email}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col sm={12} md={6} className="text-center">
+                        <Card className="border border-top-0 border-left-0 border-right-0">
+                            <Card.Body>
+                                {contacts.address}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Container>
         </section>
     )
