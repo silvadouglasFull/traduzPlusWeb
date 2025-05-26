7fc1cd041cd0d43b4100d32343b318e3f03dd1d8
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-26 15:48:04 -0300

    feat: refactor FirstSection layout to display contact information using cards

--- src/pages/aboutUs/layouts/firstSection/index.tsx
+++ src/pages/aboutUs/layouts/firstSection/index.tsx
@@ -1,34 +1,31 @@
-import { contacts } from "@flavor/constants/contacts";
-import { formatPhoneNumber } from "@utils/form/mask/phone";
+import { AnimationSlide } from "@components/animationSlide/slideLeftRight";
+import { imageFirstSection } from "@flavor/constants/assets/firstSection";
+import { paragraphs } from "@flavor/constants/texts/about/firstSection";
+import { Image } from "@pages/components/image";
+import { Paragraph } from "@pages/components/paragraph";
+import { SubTitle } from "@pages/components/subTitle";
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 export const FirstSection: React.FC = () => {
     return (
         <section className="mb-3">
             <Container>
-                <Row className="mt-5 justify-content-center justify-content-lg-between flex-lg-row-reverse align-items-center">
-                    {contacts.phones.map(item => (
-                        <Col className="text-center" sm={12} md={6} key={item.key}>
-                            <Card className="border border-top-0 border-left-0 border-right-0">
-                                <Card.Body>
-                                    {formatPhoneNumber(item.number, "br")}
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                    <Col sm={12} md={6} className="text-center">
-                        <Card className="border border-top-0 border-left-0 border-right-0">
-                            <Card.Body>
-                                {contacts.email}
-                            </Card.Body>
-                        </Card>
+                <Row className="justify-content-center justify-content-lg-between flex-lg-row-reverse align-items-center">
+                    <Col md={12} className="mt-5 mb-3" lg={6} xxl={5} sm={12}>
+                        <SubTitle text="overview" />
+                        <p className="font-weight-bold">Noster pars vix falleres valebat est. Ire velox ducunt ad ferox zirbus.</p>
+                        <AnimationSlide direction="right">
+                            <Paragraph items={paragraphs} />
+                            <Button variant="oxford" className="w-100 mb-5 text-uppercase" size="lg">
+                                <Link to={'/services'} className="text-decoration-none text-light">
+                                    View our Services
+                                </Link>
+                            </Button>
+                        </AnimationSlide>
                     </Col>
-                    <Col sm={12} md={6} className="text-center">
-                        <Card className="border border-top-0 border-left-0 border-right-0">
-                            <Card.Body>
-                                {contacts.address}
-                            </Card.Body>
-                        </Card>
+                    <Col md={12} lg={6} xxl={7} sm={12} className="text-center">
+                        <Image src={imageFirstSection} />
                     </Col>
                 </Row>
             </Container>
