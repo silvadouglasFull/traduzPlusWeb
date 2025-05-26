30e40210b937645ab7a054b43c73c5ba50e5b282
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-26 17:43:04 -0300

    feat: remove isActive property from language items in languages array

--- src/utils/languages/index.ts
+++ src/utils/languages/index.ts
@@ -5,21 +5,18 @@ export const languages: Array<LanguageItemProps> = [
         id: 0,
         icon: '🇺🇸',
         label: 'English',
-        isActive: true,
         language: 'en',
     },
     {
         id: 1,
         icon: '🇧🇷',
         label: 'Português',
-        isActive: false,
         language: 'pt'
     },
     {
         id: 2,
         icon: '🇨🇳',
         label: '中文',
-        isActive: false,
         language: '缩写'
     }
 ]
