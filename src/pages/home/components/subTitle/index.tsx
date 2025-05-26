d6f72041ef5035005caa09d0b8483d0408eb0e28
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-05-26 10:55:31 -0300

    feat: refactor SubTitle component to apply styles from a separate styles file and clean up inline styles

--- src/pages/home/components/subTitle/index.tsx
+++ src/pages/home/components/subTitle/index.tsx
@@ -4,15 +4,8 @@ import { transformerInFirstWord } from "@utils/transfomerText";
 import type React from "react";
 export const SubTitle: React.FC<SubTitleProps> = ({ text, applyShadown = true, ...props }: SubTitleProps) => {
     return (
-        <h2 style={styles} {...props} className="text-uppercase font-weight-bold text-center mb-5">
-            <span style={{
-                fontSize: '4rem',
-                opacity: 0.2,
-                left: 25,
-                position: 'relative',
-                top: 10
-
-            }}>{applyShadown ? transformerInFirstWord({ world: text }) : null}</span>
+        <h2 {...props} className="text-uppercase font-weight-bold text-center mb-5">
+            <span style={styles}>{applyShadown ? transformerInFirstWord({ world: text }) : null}</span>
             {text}
         </h2>
     )
--- src/pages/home/components/subTitle/styles.ts
+++ src/pages/home/components/subTitle/styles.ts
@@ -1,4 +1,9 @@
 import type { Styles } from "@pagesHome/components/subTitle/types";
 export const styles: Styles = {
+    fontSize: '4rem',
+    opacity: 0.2,
+    left: 25,
     position: 'relative',
+    top: 10
+
 }
