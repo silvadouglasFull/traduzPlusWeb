75fdcf0a11dd4405d986071a9a6e7399019193e6
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-23 18:07:00 -0300

    feat: update ClassroomLearningMoment image path for consistency

--- src/flavor/constants/assets/services/secoundSection/index.ts
+++ src/flavor/constants/assets/services/secoundSection/index.ts
@@ -3,7 +3,7 @@ import { VITE_CLOUD_FLARE } from "@config/index"
 const BussinesPresentation = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/BussinesPresentation.png`
 const ChinaBusinessConsulting = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/ChinaBusinessConsulting.png`
 const ChinesEscritorioBrasil = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/ChinesEscritorioBrasil.png`
-const ClassroomLearningMoment = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/ClassroomLearningMoment.png`
+const ClassroomLearningMoment = `${VITE_CLOUD_FLARE}/greatNewSoluctions/swiper/1.png`
 const HomemSentadoNoSofa = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/HomemSentadoNoSofa.jpg`
 const OfficeWorkFocus = `${VITE_CLOUD_FLARE}/greatNewSoluctions/images/services/OfficeWorkFocus.png`
 export {
