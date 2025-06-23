52ca6d4c9706484b33031f2d7075ecc150a49042
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-23 18:12:51 -0300

    feat: update language dropdown text color based on navbar background

--- src/components/navbarPublic/languageDropdown/button/index.tsx
+++ src/components/navbarPublic/languageDropdown/button/index.tsx
@@ -1,11 +1,14 @@
+import { useNavbarBackground } from "@components/navbarPublic/hooks/useNavbarBackground";
 import { useLanguage } from "@context/language/hooks";
 import React from "react";
 import { NavDropdown } from "react-bootstrap";
 
 export const Button: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const { icon } = useLanguage()
+    const { hasBackground } = useNavbarBackground()
+
     return (
-        <NavDropdown title={<span className="text-light">{icon} Languages</span>} id="collapsible-nav-dropdown">
+        <NavDropdown title={<span className={`text-${hasBackground ? 'dark' : 'light'}`}>{icon} Languages</span>} id="collapsible-nav-dropdown">
             {children}
         </NavDropdown>
 
