19f6ea9477739102b2b6b04ebfc4614127cbc780
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-23 17:51:56 -0300

    feat: update Navbar link styles to adapt based on background presence

--- src/components/navbarPublic/links/item/index.tsx
+++ src/components/navbarPublic/links/item/index.tsx
@@ -1,11 +1,14 @@
+import { useNavbarBackground } from "@components/navbarPublic/hooks/useNavbarBackground";
 import type { LinkItem } from "@components/navbarPublic/links/item/types";
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Item: React.FC<LinkItem> = ({ title, url, icon, isActive }) => {
+    const { hasBackground } = useNavbarBackground()
+
     return (
-        <Nav.Link as={Link} className='text-light me-5' to={url} active={isActive}>
+        <Nav.Link as={Link} className={`text-${hasBackground ? 'dark' : 'light'} me-5`} to={url} active={isActive}>
             {icon ? icon : null} {title}
         </Nav.Link>
     )
