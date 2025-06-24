c24a3c9590f649cb2f2d70f738e2cc45c7a9a2b7
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-24 14:51:50 -0300

    refactor: simplify language dropdown button by removing redundant NavDropdown components and unused hook feat: add responsive background color style for small screens

--- src/components/navbarPublic/languageDropdown/button/index.tsx
+++ src/components/navbarPublic/languageDropdown/button/index.tsx
@@ -1,20 +1,13 @@
-import { useNavbarBackground } from "@components/navbarPublic/hooks/useNavbarBackground";
 import { useLanguage } from "@context/language/hooks";
 import React from "react";
 import { NavDropdown } from "react-bootstrap";
 
 export const Button: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     const { icon } = useLanguage()
-    const { hasBackground } = useNavbarBackground()
 
     return (
-        <>
-            <NavDropdown className="d-block d-md-none" title={<span className="text-light">{icon} Languages</span>} id="collapsible-nav-dropdown">
-                {children}
-            </NavDropdown>
-            <NavDropdown className="d-none d-md-block" title={<span className={`text-${hasBackground ? 'dark' : 'light'}`}>{icon} Languages</span>} id="collapsible-nav-dropdown">
-                {children}
-            </NavDropdown>
-        </>
+        <NavDropdown title={<span className="text-light">{icon} Languages</span>} id="collapsible-nav-dropdown">
+            {children}
+        </NavDropdown>
     )
 }
