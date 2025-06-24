f3483b0a70f9fcd2ed509268c3d7f1e76e71181a
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-06-24 14:47:03 -0300

    refactor: simplify Navbar item component by removing unused background hook and redundant links

--- src/components/navbarPublic/links/item/index.tsx
+++ src/components/navbarPublic/links/item/index.tsx
@@ -1,21 +1,14 @@
-import { useNavbarBackground } from "@components/navbarPublic/hooks/useNavbarBackground";
 import type { LinkItem } from "@components/navbarPublic/links/item/types";
 import React from "react";
 import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const Item: React.FC<LinkItem> = ({ title, url, icon, isActive }) => {
-    const { hasBackground } = useNavbarBackground()
 
     return (
-        <>
-            <Nav.Link as={Link} className={`d-none d-md-block mt-4 text-${hasBackground ? 'dark' : 'light'} me-5`} to={url} active={isActive}>
-                {icon ? icon : null} {title}
-            </Nav.Link>
-            <Nav.Link as={Link} className={`d-block d-md-none mt-4 text-light me-5`} to={url} active={isActive}>
-                {icon ? icon : null} {title}
-            </Nav.Link>
-        </>
+        <Nav.Link as={Link} className="mt-4 text-light me-5" to={url} active={isActive}>
+            {icon ? icon : null} {title}
+        </Nav.Link>
     )
 }
 export { Item };
