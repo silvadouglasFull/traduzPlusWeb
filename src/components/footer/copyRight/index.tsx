4dd28fd13fcf283aceec3093609fa8c0778d9b94
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-02 07:52:59 -0300

    feat link copy right

--- src/components/footer/copyRight/index.tsx
+++ src/components/footer/copyRight/index.tsx
@@ -1,32 +1,29 @@
-import { logoDSWeb } from "@constants/assets";
 import { linkDSWeb } from "@constants/index";
 import { appVersion } from "@flavor/index";
 import { openPage } from "@utils/openPages";
 import React from "react";
-import { Col, Image, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export const CopyRight: React.FC = () => {
     const handleOpenLink = () => {
         openPage(linkDSWeb)
     }
     return (
-        <Row className="text-center">
-            <Col className="col-12 mb-3">
-                <Image
-                    className="rounded-4"
-                    style={{ width: 32, height: 32, cursor: 'pointer' }}
-                    alt="logo da empresa ds web criada do layout do site"
-                    src={logoDSWeb}
-                    onClick={handleOpenLink}
-                />
+        <Row>
+            <Col className="mb-3">
+                <Row className="d-flex justify-content-center align-items-center flex-nowrap">
+                    <Link to={'#'} onClick={handleOpenLink} className="text-light text-decoration-none">
+                        <small>Desenvolvido por DS Web</small>
+                    </Link>
+                </Row>
             </Col>
-            <Col className="col-8">
-                <Link to={'#'} className="text-light text-decoration-none mt-3">Politica de Privacidade</Link>
-            </Col>
-            <Col className="col-4">
+            <Col>
                 <Link to={'#'} className="text-light text-decoration-none mt-3">
-                    <strong>Versão do site:</strong> {appVersion}
+                    <small>v{appVersion}</small>
                 </Link>
+            </Col>
+            <Col>
+                <Link to={'#'} className="text-light text-decoration-none mt-3">Politica de Privacidade</Link>
             </Col>
         </Row>
     )
