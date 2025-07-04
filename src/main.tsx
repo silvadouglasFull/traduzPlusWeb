1a2b6a8aafe78d7ee8349585240a981314c06c4c
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-04 18:23:43 -0300

    configurado gtag

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { container as GoogleTagManagerService } from "@services/GoogleTagManagerService/container";
+import { gtag } from '@flavor/constants/gtag/index.ts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import TagManager from 'react-gtm-module';
 import { App } from "./App.tsx";
-GoogleTagManagerService().init()
+
+const tagManagerArgs = {
+  gtmId: gtag,
+};
+
+TagManager.initialize(tagManagerArgs);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
--- src/services/GoogleTagManagerService/container/index.ts
+++
@@ -1,5 +0,0 @@
-import { gtag } from "@flavor/constants/gtag"
-import { GoogleTagManagerService } from "@services/GoogleTagManagerService"
-export const container = () => {
-    return new GoogleTagManagerService(gtag)
-}
